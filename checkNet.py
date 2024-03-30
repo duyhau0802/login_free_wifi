00729821446c1bc1b8e2d0f9600b6fbe577a54cf
Nguyễn Duy Hậu <nguyen.duy.hau@sun-asterisk.com>
2024-03-30 15:53:59 +0700

    fix checkNet.py (run in headless option, build exe file and test run checkNet.exxe and freeWifi.exe )

--- checkNet.py
+++ checkNet.py
@@ -1,13 +1,16 @@
 import subprocess
+import time
+import os
 
 def check_internet_connection():
     try:
         # Thử ping đến Default Gateway
         result = subprocess.run(['ping', '172.172.0.1', '-n', '1'], capture_output=True, text=True, timeout=5)
-        if "Destination host unreachable" in result.stdout or "Destination net unreachable" or "time out" in result.stdout:
+        if "Destination host unreachable" in result.stdout or "Destination net unreachable" or "Request timed out" in result.stdout:
             print("Destination net unreachable. Running the executable program.")
             # Chạy chương trình .exe 
-            subprocess.run(["./dist/freeWifi.exe"])
+            subprocess.run(["./dist/freeWifi.exe"], stderr=subprocess.PIPE)
+            return False
         else:
             print('connected')
             return True
@@ -15,5 +18,13 @@ def check_internet_connection():
         print('timeout')
         return False
 
-
-check_internet_connection()
+# Run indefinitely
+while True:
+    # Check internet connection
+    if check_internet_connection():
+        print("Internet connected!")
+    else:
+        print("No internet connection!")
+    
+    # Wait for 15 minutes before checking again
+    time.sleep(15 * 60 + 2)  # 15 minutes + 5 second delay
--- freeWifi.py
+++ freeWifi.py
@@ -1,20 +1,29 @@
+import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.edge.options import Options
 from selenium.webdriver.common.action_chains import ActionChains
-
-
+from selenium.webdriver.edge.service import Service
 import time
 
 def auto_wireless_login():
+    driver = None
     try:
         edge_options = Options()
         profile_path = r"C:\Users\WIN-PRO\AppData\Local\Microsoft\Edge\User Data\Profile 1"
         edge_options.add_argument(f"user-data-dir={profile_path}")
+        edge_options.add_argument("headless")
+        edge_options.add_argument('--disable-gpu')  # Vô hiệu hóa GPU (khuyến nghị khi chạy headless)
+        edge_options.add_argument('--log-level=3')  # Giảm số lượng thông báo log (Chỉ hoạt động với Chrome, nhưng thử nếu bạn sử dụng bản dựa trên Chromium)
+        edge_options.add_argument('--silent')  # Cố gắng chạy trình điều khiển mà không xuất log ra console
+        edge_options.add_experimental_option('excludeSwitches', ['enable-logging'])  # Loại bỏ thông báo "DevTools listening on..."
+        
+        # ngăn log error ra cmd
+        service = Service(log_path=os.devnull)
 
         # Khởi tạo WebDriver với các tùy chọn đã cấu hình
-        driver = webdriver.Edge(options=edge_options)
+        driver = webdriver.Edge(options=edge_options, service=service)
 
         driver.get("http://acm.awingconnect.vn/login?serial=4C:5E:0C:05:00:40&client_mac=34:F3:9A:6C:44:B8&client_ip=172.172.30.204&userurl=http://www.msftconnecttest.com/redirect&login_url=http://free.wi-mesh.vn/login")
         time.sleep(4)  # Đợi trang web load
@@ -26,9 +35,11 @@ def auto_wireless_login():
         # Tắt âm thanh của tab bằng cách chạy JavaScript trong trình duyệt
         driver.execute_script("var videos = document.getElementsByTagName('video'); for (var i = 0; i < videos.length; i++) {videos[i].muted = true;}")
 
+        # Cuộn xuống dưới cùng của trang
+        driver.find_element(By.TAG_NAME,"body").send_keys(Keys.PAGE_DOWN)
+
         # Đợi 6s
         time.sleep(7)
-        # driver.execute_script(f"console.log('Giá trị của src_value là: {src_value}')")
 
         connect_button = driver.find_element(By.ID, "connectToInternet")
         connect_button.click()
@@ -41,7 +52,8 @@ def auto_wireless_login():
     except Exception as e:
         print("Đã xảy ra lỗi:", e)
     finally:
-        driver.quit()
+        if driver:
+            driver.quit()
 
 # Thực hiện chương trình
 auto_wireless_login()
